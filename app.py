4d7ddd8ccd97fd1ab944892229aee86d6dce776a
JMBorines14 <jamesborines@gmail.com>
2022-12-05 14:51:49 +0800

    Added Flask base

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_restful import Api, Resource
+import mysql.connector, os
+
+#initialize database connection
+mydb = mysql.connector.connect(
+    host = os.environ["host"],
+    user = os.environ["username"],
+    password = os.environ["password"]
+)
+
+app = Flask(__name__)
+api = Api(app)
+
+class Feedback(Resource):
+    def get(self): #read
+        pass
+
+    def put(self): #create
+        pass
+
+    def post(self): #update
+        pass
+
+    def delete(self): #delete
+        pass
+
+if __name__ == "__main__":
+    app.run(debug = True)
